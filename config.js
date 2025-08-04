b3cf1de77aa7d210c73dd33c0c5eac9822369ec4
quangnamttp <quangnamttp@gmail.com>
2025-08-04 21:08:23 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
   // Token bot Telegram (bạn đang dùng bot cá nhân nên để trực tiếp cũng được)
-  TELEGRAM_BOT_TOKEN: '8046421418:AAEAFt3dBzScgeFrNxdYrikyZxLOLD8kcIY',
+  TELEGRAM_BOT_TOKEN: '8046421418:AAHw1cxPAv43wXB7rZa-44PxTsV8wQT5eQ0',
 
   // ID người dùng Telegram của bạn (bạn có thể tìm bằng cách nhắn cho @userinfobot)
   TELEGRAM_USER_IDS: ['5335165612'],
