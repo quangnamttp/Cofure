4d36d6c9e8beeb347370e4f61817699857e77ac4
quangnamttp <quangnamttp@gmail.com>
2025-08-04 12:11:12 +0700

    Create config.js

---
+++ config.js
@@ -0,0 +1,13 @@
+// config.js
+module.exports = {
+  TELEGRAM_BOT_TOKEN: '8046421418:AAEAFt3dBzScgeFrNxdYrikyZxLOLD8kcIY',  
+  TELEGRAM_USER_IDS: ['123456789'],                  // ← Telegram user ID của bạn (sẽ hướng dẫn cách lấy)
+  TIMEZONE: 'Asia/Ho_Chi_Minh',
+  
+  // Bật/tắt từng module nếu cần
+  ENABLE_MORNING: true,
+  ENABLE_MACRO: true,
+  ENABLE_30MIN_SIGNAL: true,
+  ENABLE_ALERT_SIGNAL: true,
+  ENABLE_DAILY_SUMMARY: true
+};
