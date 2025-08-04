27b9e664444d7462e1620ff77aef8b2f3e07a478
quangnamttp <quangnamttp@gmail.com>
2025-08-04 18:57:41 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 // index.js
-
 const TelegramBot = require('node-telegram-bot-api');
 const { TELEGRAM_BOT_TOKEN, TELEGRAM_USER_IDS, TIMEZONE } = require('./config');
 const dayjs = require('dayjs');
+const utc = require('dayjs/plugin/utc');
+const timezone = require('dayjs/plugin/timezone');
 require('dayjs/locale/vi');
+
+// Cấu hình múi giờ
 dayjs.locale('vi');
+dayjs.extend(utc);
+dayjs.extend(timezone);
 
 const bot = new TelegramBot(TELEGRAM_BOT_TOKEN, { polling: true });
 console.log('🤖 Cofure bot đã khởi động!');
@@ -21,8 +26,10 @@ bot.onText(/\/start/, (msg) => {
   const chatId = msg.chat.id;
   const userId = msg.from.id.toString();
 
+  const timeVN = dayjs().tz(TIMEZONE).format('HH:mm:ss');
+
   if (TELEGRAM_USER_IDS.includes(userId)) {
-    bot.sendMessage(chatId, `✅ Xin chào! Cofure bot đã sẵn sàng hoạt động vào lúc ${dayjs().format('HH:mm:ss')}!`);
+    bot.sendMessage(chatId, `✅ Xin chào! Cofure bot đã sẵn sàng hoạt động vào lúc ${timeVN}!`);
   } else {
     bot.sendMessage(chatId, `🚫 Bạn không có quyền sử dụng bot này.`);
   }
