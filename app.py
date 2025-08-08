1e83c0126100392477b8a9a71aee377afcd477b3
quangnamttp <quangnamttp@gmail.com>
2025-08-08 20:05:32 +0700

    Update app.py

--- app.py
+++ app.py
@@ -52,3 +52,11 @@ async def main():
         await application.stop()
         await application.shutdown()
         await runner.cleanup()
+import asyncio
+from cofure_bot.main import main
+
+if __name__ == "__main__":
+    try:
+        asyncio.run(main())
+    except KeyboardInterrupt:
+        pass
